9fec74ca897a543f416d119df11201200c063dde
Ekaterina_Oleg <ear_cooking@mail.ru>
2019-02-10 13:17:49 +0000

    example can be used with different methods

--- ssl_api.py
+++ ssl_api.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 app = Flask(__name__)
 
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    with open('default_response.json') as f:
-        default_response = json.load(f)
+    if request.method == 'GET':
+        return jsonify("Hello!\n")
+    if request.method == 'POST':
+		
+        with open('default_response.json') as f:
+            default_response = json.load(f)
 
-    return jsonify(default_response)
+        return jsonify(default_response)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', ssl_context='adhoc')
