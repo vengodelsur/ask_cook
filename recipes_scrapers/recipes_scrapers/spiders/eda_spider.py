0526b6c0383de0d5d48374595cba64b6ac8e3d07
vengodelsur <ekatlmsh@yandex.ru>
2019-02-24 16:29:01 +0300

    modify eda.ru spider add entity 'steps'

--- recipes_scrapers/recipes_scrapers/spiders/eda_spider.py
+++ recipes_scrapers/recipes_scrapers/spiders/eda_spider.py
@@ -1,18 +1,22 @@
-import scrapy
+mport scrapy
+
 
 class EdaSpider(scrapy.Spider):
     """ scrapy crawl eda -a start_url="https://eda.ru/recepty/zakuski/brusketta-s-pomidorami-29566" """
 
     name = "eda"
-    
-    def __init__(self, *args, **kwargs): 
-        super(EdaSpider, self).__init__(*args, **kwargs) 
 
-        self.start_urls = [kwargs.get('start_url')] 
+    def __init__(self, *args, **kwargs):
+        super(EdaSpider, self).__init__(*args, **kwargs)
+
+        self.start_urls = [kwargs.get('start_url')]
 
     def parse(self, response):
         yield {
             'url': response.request.url,
             'recipe_title': response.css('body > div.wrapper-sel > section > section > div.recipe__title h1.recipe__name::text').get(),
-            'ingredients': response.css('body > div.wrapper-sel > section > section > div.g-relative.js-responsive-banner-relative > div.ingredients-list.layout__content-col > div.ingredients-list__content p::attr(data-ingredient-object)').getall()
+            'ingredients': response.css('body > div.wrapper-sel > section > section > div.g-relative.js-responsive-banner-relative > div.ingredients-list.layout__content-col > div.ingredients-list__content p::attr(data-ingredient-object)').getall(),
+            'steps': response.css('body > div.wrapper-sel > section > section > div.recipe__instruction > ul.recipe__steps li > div.instruction__wrap > span.instruction__description::text').getall()
+
         }
+
