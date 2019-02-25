604ca6ecff0e63b7287b8a4b7e5973e728fd6808
vengodelsur <ekatlmsh@yandex.ru>
2019-02-25 11:46:34 +0300

    autopep8 eda.ru and say7.info spiders

--- recipes_scrapers/recipes_scrapers/spiders/edaru_spider.py
+++ recipes_scrapers/recipes_scrapers/spiders/edaru_spider.py
@@ -2,10 +2,10 @@ import scrapy
 
 
 class EdaRuSpider(scrapy.Spider):
+
     """ scrapy crawl eda -a start_url="https://eda.ru/recepty/zakuski/brusketta-s-pomidorami-29566" """
 
     name = "EdaRu"
-    
 
     def __init__(self, *args, **kwargs):
         super(EdaRuSpider, self).__init__(*args, **kwargs)
@@ -15,8 +15,8 @@ class EdaRuSpider(scrapy.Spider):
     def parse(self, response):
 
         selectors = {'recipe_title': 'div.recipe__title h1.recipe__name::text',
-     'ingredients': 'div.ingredients-list__content p::attr(data-ingredient-object)',
-     'steps': 'ul.recipe__steps li > div.instruction__wrap > span.instruction__description::text'}
+                     'ingredients': 'div.ingredients-list__content p::attr(data-ingredient-object)',
+                     'steps': 'ul.recipe__steps li > div.instruction__wrap > span.instruction__description::text'}
         yield {
             'url': response.request.url,
             'recipe_title': response.css(selectors['recipe_title']).get(),
@@ -24,4 +24,3 @@ class EdaRuSpider(scrapy.Spider):
             'steps': response.css(selectors['steps']).getall()
 
         }
-
