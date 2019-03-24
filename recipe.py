aca093a077de42b31da30c9c03ebc0a4ecc84c0f
vengodelsur <ekatlmsh@yandex.ru>
2019-03-24 12:24:19 +0300

    add basic string representation of recipe see recipe.py

--- recipe.py
+++ recipe.py
@@ -13,6 +13,7 @@ class Recipe:
        self.title = json_recipe['recipe_title']
        self.ingredients = json_recipe['ingredients']
        self.steps = json_recipe['steps']
+       self.normalize_title()
        self.normalize_steps()
 
     def __iter__(self):
@@ -20,16 +21,33 @@ class Recipe:
         yield 'title', self.title
         yield 'ingredients', self.ingredients
         yield 'steps', self.steps
+
+    def normalize_title(self):
+        self.title = self.title.strip()
             
     def normalize_steps(self):
         self.steps = [remove_multiple_spaces(step) for step in self.steps]         
         self.steps = [step for step in self.steps if step]
+
+   
     
     def dump(self, filename):
+        
         with open(filename, 'w') as f:
             json.dump(dict(self), f)
+
+    def steps_to_string(self):
+        return '\n\n'.join(self.steps)
+
+    def ingredients_to_string(self):
+        return '\n'.join([ingredient for ingredient in self.ingredients])
+
+    def __str__(self):        
+        steps_string = self.steps_to_string()
+        return '\n\n'.join([self.url, self.title, self.ingredients_to_string(),  self.steps_to_string()])
 
 if __name__ == '__main__':
     recipe = Recipe('data/eda_ru.json')
     recipe.dump('data/output.json')
-    print(recipe.steps)
+    
+    print(recipe)
